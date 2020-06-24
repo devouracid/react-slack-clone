5502ba2d822ed708fba6c070016cb25c5586f693
David Cuero <W5845542@student.hccs.edu>
2020-06-24 01:12:27 -0500

    React-Slack-Clone
    First Commit

---
+++ src/components/firebase.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app';
+import "firebase/auth";
+import "firebase/database";
+import "firebase/storage";
+
+var firebaseConfig = {
+    apiKey: "AIzaSyAyUsBLsx6P9eT4Y4QYiMcQp5AkEYwH0n8",
+    authDomain: "react-slack-clone-cf295.firebaseapp.com",
+    databaseURL: "https://react-slack-clone-cf295.firebaseio.com",
+    projectId: "react-slack-clone-cf295",
+    storageBucket: "react-slack-clone-cf295.appspot.com",
+    messagingSenderId: "922664530055",
+    appId: "1:922664530055:web:2d0489dac1465c83084822",
+    measurementId: "G-L3ZY014T9J"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
+  export default firebase;
